6891f1f47172de2d47da2dc169e80cccd0bffdca
Vivius <peillex.vincent@live.fr>
2016-12-11 13:42:05 +0100

    Initial commit

---
+++ src/info530/Main.java
@@ -0,0 +1,8 @@
+package info530;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/info530/Pile.java
@@ -0,0 +1,14 @@
+package info530;
+
+import java.util.Stack;
+
+/**
+ * Created by Vincent on 11/12/2016.
+ */
+public class Pile {
+    private Stack<Integer> pile;
+
+    public void put(Integer e) {
+
+    }
+}
